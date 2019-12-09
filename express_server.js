6c2177168f1223a31a7d37cacd1e373dea1e9316
Channimsheung <msj@live.hk>
2019-12-09 04:12:27 +0000

    added the template

--- express_server.js
+++ express_server.js
@@ -8,6 +8,11 @@ const urlDatabase = {
 
 app.set("view engine", "ejs");
 
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
