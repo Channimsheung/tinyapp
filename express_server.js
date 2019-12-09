987f4c93edf60610b0f1650b9845cfe4ee873e81
Channimsheung <msj@live.hk>
2019-12-09 19:07:57 +0000

    made two new files

--- express_server.js
+++ express_server.js
@@ -2,21 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port 8080
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
+  b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 app.set("view engine", "ejs");
-
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
-app.get("/urls:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: (href = "#") };
-  res.render("urls_show", templateVars);
-});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -28,6 +18,17 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let shortUrl = req.params.shortURL;
+  let templateVars = { shortURL: shortUrl, longURL: urlDatabase[shortUrl] };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
